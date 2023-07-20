3f397bf0a9692d832a57999042da5a8dc0084d7e
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-07-20 15:05:57 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return "Inicio Home";
+    return view('welcome');
 });
+
+// Route::get('/', function () {
+//     return "Inicio Home";
+// });
 
 Route::get('/contacto', function () {
     return "Este es el formulario de contacto";

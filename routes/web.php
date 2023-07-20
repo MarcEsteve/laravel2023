115af70d07dfb0fb58af36f11d7d7837b352d73e
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-07-20 15:52:06 +0200

    Ejemplo Controller

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EjemploController;
 /*
-|--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/inicio', [EjemploController::class, 'inicio'] );
+
+
 // Route::get('/', function () {
 //     return "Inicio Home";
 // });
 
-Route::get('/contacto', function () {
-    return "Este es el formulario de contacto";
-});
+// Route::get('/contacto', function () {
+//     return "Este es el formulario de contacto";
+// });
 
-Route::get('/cursos', function () {
-    return "Esta es la página de cursos";
-});
+// Route::get('/cursos', function () {
+//     return "Esta es la página de cursos";
+// });
 
 //Rutas con parámetros
 // Route::get('/post/{id}', function ($id) {
 //     return "Esta es la publicación con la id: " . $id;
 // });
 
-Route::get('/post/{id}/{nombre}', function ($id, $nombre) {
-    return "Esta es la publicación con la id: " . $id . " y el nombre del autor es: " . $nombre;
-})->where('nombre', '[a-zA-Z]+');
+// Route::get('/post/{id}/{nombre}', function ($id, $nombre) {
+//     return "Esta es la publicación con la id: " . $id . " y el nombre del autor es: " . $nombre;
+// })->where('nombre', '[a-zA-Z]+');
 
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
+// Route::get('/greeting', function () {
+//     return 'Hello World';
+// });
